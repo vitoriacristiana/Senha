f35db0e18f8af643fe9e017208b8eba0da800d8c
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-10-24 16:40:57 -0300

    resolução exercício

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string  senhaArmazenada, senhaDigitada;
+
+          senhaArmazenada  = "1234abcd";
+          Console.Write("Olá,usuário Por favor, digite sua senha:");
+          senhaDigitada  = Console.ReadLine();
+
+            if (senhaDigitada == senhaArmazenada)
+            Console.WriteLine ("Acesso permitido");
+            
+            { 
+            Console.WriteLine ("Acesso Negado");
+          }
+
+          
+
+            
+        }
+    }
+}
